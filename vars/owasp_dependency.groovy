c352687c9d1cd85e6d858fdfa3923511efec577c
Umar Shaikh <157197586+umar-sk-07@users.noreply.github.com>
2026-04-23 20:26:29 +0530

    Refactor dependencyCheck to use NVD API key
    Updated dependencyCheck to use NVD API key from credentials.

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,4 +1,9 @@
-def call(){
-  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
+def call() {
+  // Use the credentials ID 'NVD_API_KEY' that you created in Jenkins
+  dependencyCheck(
+    nvdApiKey: credentials('OWASP_key'), 
+    additionalArguments: '--scan ./', 
+    odcInstallation: 'OWASP'
+  )
   dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
