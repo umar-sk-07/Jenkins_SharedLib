bffcea0ef9caae8cf596b352b627eb5c974a96cc
Umar Shaikh <157197586+umar-sk-07@users.noreply.github.com>
2026-04-23 20:28:50 +0530

    Fix credentials ID case for OWASP key

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,7 +1,7 @@
 def call() {
   // Use the credentials ID 'NVD_API_KEY' that you created in Jenkins
   dependencyCheck(
-    nvdApiKey: credentials('OWASP_key'), 
+    nvdApiKey: credentials('OWASP_Key'), 
     additionalArguments: '--scan ./', 
     odcInstallation: 'OWASP'
   )
