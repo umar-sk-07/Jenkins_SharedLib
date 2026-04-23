c1bcad239d412faba1f96f17dfd41c8847d09418
Umar Shaikh <157197586+umar-sk-07@users.noreply.github.com>
2026-04-23 20:35:36 +0530

    Update OWASP dependency script for key handling
    Refactor dependencyCheck call to use single quotes for NVD_KEY interpolation.

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,14 +1,10 @@
 def call() {
-  // 1. Fetch the secret key from Jenkins and store it in the variable 'NVD_KEY'
   withCredentials([string(credentialsId: 'OWASP_Key', variable: 'NVD_KEY')]) {
-    
-    // 2. Pass the key as a manual argument in the additionalArguments string
     dependencyCheck(
-      additionalArguments: "--scan ./ --nvdApiKey ${NVD_KEY}", 
+      // Using single quotes (') prevents the "interpolation" warning
+      additionalArguments: '--scan ./ --nvdApiKey ' + NVD_KEY, 
       odcInstallation: 'OWASP'
     )
   }
-  
-  // 3. Publish the results
   dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
