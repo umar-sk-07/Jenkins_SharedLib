9a672f3bcdc966540ff73630a7fa9e8b270a692c
Umar Shaikh <157197586+umar-sk-07@users.noreply.github.com>
2026-04-24 11:58:49 +0530

    Modify dependencyCheck arguments for efficiency
    Updated additional arguments for dependencyCheck to improve performance and avoid 503 errors.

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,8 +1,9 @@
 def call() {
   withCredentials([string(credentialsId: 'OWASP_Key', variable: 'NVD_KEY')]) {
     dependencyCheck(
-      // Adding --noupdate tells it to use the local data and stop hitting the 503 error
-      additionalArguments: '--scan ./ --nvdApiKey ' + NVD_KEY + ' --noupdate', 
+      // --nvdValidForHours 24: Skips download if the last check was less than 24h ago (Massive speed boost)
+      // --nvdApiKey: Prevents 503 errors and speeds up the download when it DOES happen (Once a day)
+      additionalArguments: '--scan ./ --format XML --nvdApiKey ' + NVD_KEY + ' --nvdValidForHours 24', 
       odcInstallation: 'OWASP'
     )
   }
