8954191b8d0490771463070cefab93e19efb26d4
Umar Shaikh <157197586+umar-sk-07@users.noreply.github.com>
2026-04-29 16:34:05 +0530

    Refactor dependencyCheck arguments in owasp_dependency.groovy
    Updated additional arguments for dependencyCheck to include new options for NVD API key and data path.

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,11 +1,21 @@
 def call() {
   withCredentials([string(credentialsId: 'OWASP_Key', variable: 'NVD_KEY')]) {
     dependencyCheck(
-      // --nvdValidForHours 24: Skips download if the last check was less than 24h ago (Massive speed boost)
-      // --nvdApiKey: Prevents 503 errors and speeds up the download when it DOES happen (Once a day)
-      additionalArguments: '--scan ./ --format XML --nvdApiKey ' + NVD_KEY + ' --nvdValidForHours 24', 
+      /* 
+         --nvdValidForHours 168: Trusts the AMI data for 7 days (prevents the 31k record sync).
+         --autoUpdate true: Keeps updates enabled, but the 168h window will skip them for now.
+         --data: Points directly to the path we found on your Amazon Linux instance.
+      */
+      additionalArguments: """
+        --scan ./ 
+        --format XML 
+        --nvdApiKey ${NVD_KEY} 
+        --nvdValidForHours 168 
+        --data /var/lib/jenkins/tools/org.jenkinsci.plugins.DependencyCheck.tools.DependencyCheckInstallation/OWASP/data
+      """, 
       odcInstallation: 'OWASP'
     )
   }
+  // This publishes the report to the Jenkins UI
   dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
